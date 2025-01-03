b311b816168848053ffcb4a7f9246de3780f0a58
Subbulakshmi <srisubbu1726@gmail.com>
2025-01-03 18:03:30 +0530

    website

---
+++ team/vus/@demoblaze/%resources%/scripts/jsAction_c801e3c4-983e-4b35-9963-69d74cec8c46.js
@@ -0,0 +1,11 @@
+﻿// Javascript skeleton.
+// Edit and adapt to your needs.
+// The documentation of the NeoLoad Javascript API
+// is available in the appendix of the documentation.
+
+// Get variable value from VariableManager
+var myVar = context.variableManager.getValue("Token");
+if (myVar==null) {
+        context.fail("Variable 'myVar' not found");
+}
+logger.debug("Token is"+myVar);
---
+++ team/vus/@swiggy/%resources%/scripts/jsAction_2e1bc4b8-6b0b-4c24-93a0-14c45676f5ee.js
@@ -0,0 +1,4 @@
+﻿var id = context.variableManager.getValue("id");
+
+// Display the value in the NeoLoad logs
+logger.debug("Extracted ID: " + id);
